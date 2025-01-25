915f7b1a57f48b575a296b9f6e8bc6e3858a7ed1
ArthurRufi <temparthur87@gmail.com>
2025-01-24 23:04:15 -0300

    adicionado estudos para JPA

---
+++ Spring/aulaJPA/aulaJPA/src/main/java/com/example/aulaJPA/models/AuthorModel.java
@@ -0,0 +1,36 @@
+package com.example.aulaJPA.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class AuthorModel implements Serializable {
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String author;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
